8c7a2f74e6535d64f7de25da612a888e2ed0678d
Christ1naa <168696012+Christ1naa@users.noreply.github.com>
2025-05-24 21:46:37 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+TARIFFS = {
+    "day": 2.5,   # грн за кВт
+    "night": 1.2
+}
+
+FAKE_INCREMENT = {
+    "day": 100,
+    "night": 80
+}
+
+INITIAL_DATA_PATH = "data/meter_data.json"
+HISTORY_PATH = "data/history.json"
